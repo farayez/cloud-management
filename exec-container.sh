0dfaf5ce86e04da06e93c756af15b407c76f46a3
Araf Farayez <araf.farayez@gmail.com>
2024-04-17 07:58:52 +0600

    fix exec-container.sh

--- exec-container.sh
+++ exec-container.sh
@@ -4,10 +4,16 @@
 
 # Fetch running tasks
 running_tasks=($(aws ecs list-tasks \
+    --region $aws_region \
     --cluster $ecs_cluster \
     --output text \
     --query 'taskArns' \
     --service $ecs_service))
+
+if [ -z "$running_tasks" ]; then
+    echo "There's no running task"
+    exit 0
+fi
 
 # Let user select the task to exec into
 . $home_directory/utils/selection_menu.sh
@@ -16,6 +22,7 @@ echo "Selected task: $selected_task"
 
 # Exec into container
 aws ecs execute-command \
+    --region $aws_region \
     --cluster $ecs_cluster \
     --task $selected_task \
     --container $container_name \
