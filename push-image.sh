7a9463b2863b933fc3e5cee1fba4b28dce037fe5
Araf Farayez <araf.farayez@gmail.com>
2024-09-27 04:27:23 +0600

    update push-image script

--- push-image.sh
+++ push-image.sh
@@ -2,10 +2,17 @@
 
 . ./utils/initialize.sh
 
+# Prepare tag for image
+image_tag=$(echo "${branch////"."}.latest")
+
+# Update codebase
 cd $home_directory/repos/$codebase_directory || exit 1
+git fetch -a || exit 1
 git checkout $branch || exit 1
 git pull || exit 1
+
+# Build, tag and push image
 docker build -t $image_name . || exit 1
 aws ecr get-login-password --region $ecr_aws_region | docker login --username AWS --password-stdin $ecr_url || exit 1
-docker tag $image_name:latest $image_url:$branch.latest || exit 1
-docker push $image_url:$branch.latest
+docker tag $image_name:latest $image_url:$image_tag || exit 1
+docker push $image_url:$image_tag
