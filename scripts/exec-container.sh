cba3fce630aaceb5d073ae07d40bdb8b02efd885
Araf Farayez <araf.farayez@gmail.com>
2024-10-28 15:04:24 +0600

    fix exec-container based on new process

--- scripts/exec-container.sh
+++ scripts/exec-container.sh
@@ -2,6 +2,8 @@
 
 . ./utils/initialize.sh
 
+fn_validate_variables ecs_cluster ecs_service aws_region timestamp root_directory primary_container_name
+
 # Fetch running tasks
 running_tasks=($(aws ecs list-tasks \
     --region $aws_region \
@@ -15,7 +17,7 @@ if [ -z "$running_tasks" ]; then
 fi
 
 # Let user select the task to exec into
-. $home_directory/utils/selection_menu.sh
+. $root_directory/utils/selection_menu.sh
 fn_choose_from_menu "Select running task:" selected_task "${running_tasks[@]}"
 fn_info "Selected task: $selected_task"
 
@@ -24,6 +26,6 @@ aws ecs execute-command \
     --region $aws_region \
     --cluster $ecs_cluster \
     --task $selected_task \
-    --container $container_name \
+    --container $primary_container_name \
     --interactive \
     --command "/bin/bash" || fn_fatal
