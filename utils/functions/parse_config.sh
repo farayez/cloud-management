344f0120aa7c081dc842a26b6d07e75f7b586f8b
Araf Farayez <araf.farayez@gmail.com>
2025-07-26 04:08:48 +0600

    update parse_config.sh
    - allow space in config value. the value will be quoted before the variable is exported

--- utils/functions/parse_config.sh
+++ utils/functions/parse_config.sh
@@ -15,17 +15,23 @@ function fn_populate_config_variables() {
 function fn_populate_config_variables_from_json() {
     local config_json=$1
     local config_keys_value_pairs=$(fn_get_variable_export_strings_from_json "$config_json")
-    fn_export_variables "$config_keys_value_pairs"
+
+    # Process each line separately to handle quoted values properly
+    while IFS= read -r pair; do
+        if [[ -n "$pair" ]]; then
+            export "$pair"
+        fi
+    done <<< "$config_keys_value_pairs"
 }
 
 function fn_get_variable_export_strings_from_json() {
     # Get .config object > get keys value pairs > filter out null values > prepare for export
-    echo $@ | jq -r '.config | to_entries | .[] | select(.value != null and .value != {}) | "" + .key + "=" + "" + (.value | tostring) + "" '
-}
-
-function fn_export_variables() {
-    local pair
-    for pair in $@; do
-        export $pair
-    done
+    # This function returns strings in the format: key="value" or key=value
+    # The values are quoted if they contain spaces
+    echo $@ | jq -r '.config | to_entries | .[] | select(.value != null and .value != {}) | 
+        if (.value | tostring | test(" ")) then
+            .key + "=" + "\"" + (.value | tostring) + "\""
+        else
+            .key + "=" + (.value | tostring)
+        end'
 }
