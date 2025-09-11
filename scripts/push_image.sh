a60dcacb8d317b7ac70c6e82736875289bf8c0e9
Araf Farayez <araf.farayez@gmail.com>
2025-09-12 03:11:35 +0600

    add build_directory config for ECR image creation

--- scripts/push_image.sh
+++ scripts/push_image.sh
@@ -34,9 +34,16 @@ fn_run git -C $root_directory/repos/$repo fetch -a || fn_fatal
 fn_run git -C $root_directory/repos/$repo checkout $branch || fn_fatal
 fn_run git -C $root_directory/repos/$repo pull || fn_fatal
 
+# Navigate to working directory if specified
+if [ -v build_directory ]; then
+    build_directory=$root_directory/repos/$repo/$build_directory
+else
+    build_directory=$root_directory/repos/$repo
+fi
+
 # Build, tag and push image
 fn_section_start "Build and push image"
-fn_run docker-build -t $image_name $root_directory/repos/$repo || fn_fatal
+fn_run docker-build -t $image_name $build_directory || fn_fatal
 aws ecr get-login-password --region $aws_region | docker login --username AWS --password-stdin $ecr_url || fn_fatal
 fn_run docker-tag $image_name:latest $image_url:$image_tag || fn_fatal
 fn_run docker-push $image_url:$image_tag
