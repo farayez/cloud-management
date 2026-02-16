e7e37efb976876d35e5f31b160b21044850dd0e7
Araf Farayez <araf.farayez@gmail.com>
2026-02-17 01:04:47 +0600

    add starting_directory parameter in ssh script

--- scripts/ssh_into_server.sh
+++ scripts/ssh_into_server.sh
@@ -4,8 +4,13 @@
 
 fn_validate_variables host username key_path
 
-# Set permissions for the SSH keyto be read-only by the user
+# Set permissions for the SSH key to be read-only by the user
 chmod 400 $root_directory/$key_path
 
 # Use the SSH key to connect to the server
-ssh $username@$host -i $root_directory/$key_path
+# If starting_directory is set, cd to it after connection
+if [ -n "$starting_directory" ]; then
+  ssh -t $username@$host -i $root_directory/$key_path "cd $starting_directory && exec \$SHELL -l"
+else
+  ssh $username@$host -i $root_directory/$key_path
+fi
